38c9fff4edbe95a6fcd342bf1e47fa6aa0c4c35c
nfrancese <nico97953@gmail.com>
2022-08-10 12:25:16 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="App">
+      <div className='section-container first-section'>
+        <div className='header'>
+          
+        </div>
+      </div>
+
+      <div className='section-container second-section'>
+        Quienes Somos
+      </div>
+
+      <div className='section-container third-section'>
+
+      </div>
+
+      <div className='section-container fourth-section'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
