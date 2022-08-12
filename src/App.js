da131be1970e889230610e1dc77740b767898e85
JuanCruz2 <Juan.Cruz.Gonzalez169@gmail.com>
2022-08-12 00:49:47 -0300

    header logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import './images/logo2.png';
 
 function App() {
   return (
     <div id="App">
       <div className='section-container first-section'>
+        
         <div className='header'>
-          
+          <img className= 'image' src='https://scontent.fmdq3-1.fna.fbcdn.net/v/t39.30808-6/228586420_349526056645048_369007878640427440_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=973b4a&_nc_ohc=peQsiRRD47UAX8Dhk_S&_nc_ht=scontent.fmdq3-1.fna&oh=00_AT8KRoQKV9wFX4M32aPrWjSBYLTezcGSTXq7slZuHGb1xg&oe=62FAD2D6'/>
+        </div>
+        <div className='father-text-div'>
+            <h1 className='font'>Find your</h1>
+        </div>
+        <div className='son-text-div'>
+            <h1 className='font'>dream car</h1>
         </div>
       </div>
 
