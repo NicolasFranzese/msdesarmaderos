a8fb2ec2a5c13ed26a05ec619ef8f695403cafe7
JuanCruz2 <Juan.Cruz.Gonzalez169@gmail.com>
2022-08-15 02:53:12 -0300

    structure changes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ function App() {
       <div className='section-container first-section'>
         
         <div className='header'>
-          <img className= 'image' src='https://scontent.fmdq3-1.fna.fbcdn.net/v/t39.30808-6/228586420_349526056645048_369007878640427440_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=973b4a&_nc_ohc=peQsiRRD47UAX8Dhk_S&_nc_ht=scontent.fmdq3-1.fna&oh=00_AT8KRoQKV9wFX4M32aPrWjSBYLTezcGSTXq7slZuHGb1xg&oe=62FAD2D6'/>
+          <img className= 'image' src='https://dvqlxo2m2q99q.cloudfront.net/000_clients/400140/page/400140WKqcxaTs.jpg'/>
         </div>
+        <div className='column-div'></div>
         <div className='father-text-div'>
             <h1 className='font'>Find your</h1>
         </div>
         <div className='son-text-div'>
             <h1 className='font'>dream car</h1>
         </div>
+        <div className='slogan-div'>
+          <h2 className='slogan-font'>We can help you find the best car. Check our reviews, compare models and find cars for sale</h2>
+        </div>
+        
       </div>
 
       <div className='section-container second-section'>
