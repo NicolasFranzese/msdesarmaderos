c967c8a92173d0a4a5fe676f66022e40b4ef69d2
JuanCruz2 <Juan.Cruz.Gonzalez169@gmail.com>
2022-08-24 01:47:19 -0300

    sponsors layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
-import logo from './images/logo.png'
-// import ford from './images/ford.png'
+import logo from './images/logo.png';
+import ford from './images/ford.png';
+import chevrolet from './images/chevrolet.png';
+import peugeot from './images/peugeot.png';
+import renault from './images/renault.png';
+import audi from './images/audi.png';
+import volkswagen from './images/volkswagen.png'
 
 function App() {
   return (
     <div id="App">
       <div className='section-container first-section'>
-        
+
         <div className='header'>
-          <img className= 'image' src={logo}/>
+          <img className='image' src={logo} />
         </div>
         <div className='home-container'>
           <div>
             <div className='father-text-div'>
               <h1 className='font'>MSDesarmaderos</h1>
             </div>
-          
+
             <div className='slogan-div'>
               <h2 className='slogan-font'>Es una empresa con grandes objetivos, la cual ha nacido de la fusión de un grupo de personas con mucha experiencia en el rubro, haciendo de la profesionalidad y un continuo afán de superación, los pilares fundamentales para llevar a cabo el cumplimiento de nuestros objetivos.</h2>
             </div>
@@ -25,24 +30,37 @@ function App() {
           </div>
         </div>
 
-       <div style={{ color: "white"}}>
-        <div>
-          Nuestras marcas
-        </div>
-        <div>
-        { /* <img className= 'ford' src={ford}/> */}
-        { /* <img className= 'ford' src={ford}/> */}
-        { /* <img className= 'ford' src={ford}/> */}
-        { /* <img className= 'ford' src={ford}/> */}
-        { /* <img className= 'ford' src={ford}/> */}
-        </div>
-       </div>
+        <div className='companies'>
+          <div className='son-companies' >
+            <h1 className='tittle-sponsors'>Nuestras Marcas </h1>
 
-        
+          </div>
+          <div className='images-companies'>
+            <img className='sponsors' src={ford} />
+            <img className='sponsors' src={chevrolet} />
+            <img className='sponsors' src={peugeot} />
+            <img className='sponsors' src={renault} />
+            <img className='sponsors' src={audi} />
+            <img className='sponsors' src={volkswagen} />
+
+          </div>
+        </div>
+
+
       </div>
 
       <div className='section-container second-section'>
-        Quienes Somos
+        {/* <div className='section-identity'>
+          <div className='tittle-identity'>Why chose us?</div>
+
+          <div className='presentation'>
+            <div>presentation1</div>
+            <div>presentation2</div>
+            <div>presentation3</div>
+           </div>
+
+
+        </div> */}
       </div>
 
       <div className='section-container third-section'>
