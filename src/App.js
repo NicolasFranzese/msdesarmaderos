6b11ebea2c2ab0257e08418b5afd349dc6d49039
JuanCruz2 <Juan.Cruz.Gonzalez169@gmail.com>
2022-10-19 23:52:56 -0300

    maps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+
 import "./App.css";
+import Iframe from 'react-iframe';
 import logo from "././images/logo.ico";
 import ford from "./images/ford.png";
 import chevrolet from "./images/chevrolet.png";
@@ -14,6 +16,7 @@ import telefono from "./images/telefono2.png";
 import maps from "./images/maps.png";
 import clean from "./images/clean.jpg"
 import clean2 from "./images/clean2.jpg"
+
 function App() {
   return (
     <div id="App">
@@ -108,7 +111,8 @@ function App() {
                 <h4 className="subtitle-shops">MAR DEL PLATA</h4>
               </div>
               <div className="maps">
-                <img className="maps-image" src={maps}></img>
+                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12577.278143685986!2d-57.58725335176667!3d-37.99300665104608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584d92f185d0925%3A0x5020da28f3ebef8e!2sAutopartes%20Raul!5e0!3m2!1ses-419!2sar!4v1666150312503!5m2!1ses-419!2sar"
+                  className="maps-image" />
               </div>
               <div className="data-maps">
                 <div className="sub-maps">
@@ -128,7 +132,8 @@ function App() {
                 <h4 className="subtitle-shops">BARCELONA</h4>
               </div>
               <div className="maps">
-                <img className="maps-image" src={maps}></img>
+                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3144.298579836346!2d-57.58071666590024!3d-37.993495532061104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584d9fde2e5ba29%3A0x65924f82b394e35b!2sIndustrias%20DVD%20GNC%20Express!5e0!3m2!1ses-419!2sar!4v1666233815964!5m2!1ses-419!2sar"
+                  className="maps-image" />
               </div>
               <div className="data-maps">
                 <div className="sub-maps">
@@ -150,7 +155,8 @@ function App() {
                 <h4 className="subtitle-shops">NAPLES</h4>
               </div>
               <div className="maps">
-                <img className="maps-image" src={maps}></img>
+                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6290.866126745319!2d-57.55386740068057!3d-37.96702166826921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584dba6beb7aa81%3A0x7c405420b007c801!2sCheverry%20Constituci%C3%B3n!5e0!3m2!1ses-419!2sar!4v1666234226164!5m2!1ses-419!2sar"
+                  className="maps-image" />
               </div>
               <div className="data-maps">
                 <div className="sub-maps">
@@ -170,7 +176,8 @@ function App() {
                 <h4 className="subtitle-shops">ROMA</h4>
               </div>
               <div className="maps">
-                <img className="maps-image" src={maps}></img>
+                <Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12577.813194054148!2d-57.56034100000001!3d-37.989886!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x48a182ef1630ca2c!2sUOCRA%20-%20Construir%20Salud%20-%20Cemap%20Mar%20del%20Plata!5e0!3m2!1ses-419!2sar!4v1666234165266!5m2!1ses-419!2sar"
+                  className="maps-image" />
               </div>
               <div className="data-maps">
                 <div className="sub-maps">
@@ -245,7 +252,9 @@ function App() {
 
       </div>
     </div>
+
   );
+
 }
 
 export default App;
