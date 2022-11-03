572619968250014168fbd7b722c6e18bc998d774
nfrancese <nico97953@gmail.com>
2022-11-03 12:36:04 -0300

    feat: avances

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
+import "./whatsapp.css";
 import "./App.css";
-import Iframe from 'react-iframe';
+import Iframe from "react-iframe";
 import logo from "././images/logo.ico";
 import ford from "./images/ford.png";
 import chevrolet from "./images/chevrolet.png";
@@ -13,9 +13,9 @@ import team from "./images/team.png";
 import finanzas from "./images/finanzas.png";
 import ubicacion from "./images/ubicacion.png";
 import telefono from "./images/telefono2.png";
-import clean from "./images/clean.jpg"
-import clean2 from "./images/clean2.jpg"
-import desarmadero from "./images/desarmadero.jpg"
+import clean from "./images/clean.jpg";
+import clean2 from "./images/clean2.jpg";
+import desarmadero from "./images/desarmadero.jpg";
 
 function App() {
   return (
@@ -42,7 +42,34 @@ function App() {
           </div>
           <div>
             <div className="image-container">
-              <img className="image-parameters" src={desarmadero}></img>
+              {/* <img className="image-parameters" src={desarmadero}></img> */}
+             
+              <a target="_blank" href="https://api.whatsapp.com/send?phone=+5492233068025&text=Hola,%20mi%20consulta%20es:" style={{ textDecoration: 'none', color: 'black'}}>
+                <div class="whatsapp-chatbox">
+                  <header>
+                    <div class="avatar"></div>
+                    <div>
+                      <h3>MSDesarmaderos</h3>
+                      <h6>
+                        En linea
+                      </h6>
+                    </div>
+                  </header>
+                  <div>
+                    <div class="chat-bubble">
+                      <p>MSDesarmaderos</p>
+                      Hola! &#128663;
+                      <br />
+                      Somos MSDesarmaderos, escribinos tu consulta.
+                    </div>
+                  </div>
+                  <footer>
+                    <a target="_blank" href="https://api.whatsapp.com/send?phone=+5492233068025&text=Hola,%20mi%20consulta%20es:">
+                      Enviar
+                    </a>
+                  </footer>
+                </div>
+              </a>
             </div>
           </div>
         </div>
@@ -58,6 +85,82 @@ function App() {
             <img className="sponsors" src={renault} />
             <img className="sponsors" src={audi} />
             <img className="sponsors" src={volkswagen} />
+          </div>
+        </div>
+      </div>
+
+      <div className="section-container fourth-section">
+        <div className="structure">
+          <div className="internal-structure">
+            <img className="clean-img" src={desarmadero}></img>
+          </div>
+          <div className="internal-structure">
+            <div className="tittle-structure">
+              <div className="content-structure">
+                <div className="distribution-structure1">
+                  <h2 className="tittle-content">
+                    CAR EXTERIOR CLEANING: BASIC DETAILING
+                  </h2>
+                </div>
+                <div className="distribution-structure2">
+                  <p className="last-description">
+                    Our technicians have undergone the most extensive and
+                    stringent car detail training program. And the only car that
+                    matters is yours because we will detail it to your complete
+                    satisfaction.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="structure">
+          <div className="internal-structure">
+            <div className="tittle-structure">
+              <div className="content-structure">
+                <div className="distribution-structure1">
+                  <h2 className="tittle-content2">
+                    RUBBING, WAXING, AND POLISHING
+                  </h2>
+                </div>
+                <div className="distribution-structure3">
+                  <p className="last-description">
+                    Our technicians have undergone the most extensive and
+                    stringent car detail training program. And the only car that
+                    matters is yours because we will detail it to your complete
+                    satisfaction.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="internal-structure">
+            {" "}
+            <img className="clean-img" src={clean2}></img>
+          </div>
+        </div>
+        <div className="structure">
+          <div className="internal-structure">
+            <img className="clean-img" src={clean}></img>
+          </div>
+          <div className="internal-structure">
+            <div className="tittle-structure">
+              <div className="content-structure">
+                <div className="distribution-structure1">
+                  <h2 className="tittle-content">
+                    CAR EXTERIOR CLEANING: BASIC DETAILING
+                  </h2>
+                </div>
+                <div className="distribution-structure2">
+                  <p className="last-description">
+                    Our technicians have undergone the most extensive and
+                    stringent car detail training program. And the only car that
+                    matters is yours because we will detail it to your complete
+                    satisfaction.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -110,158 +213,97 @@ function App() {
           <div className="shops">
             <div className="individual-shops">
               <div className="city-shops">
-                <h4 className="subtitle-shops">MAR DEL PLATA</h4>
+                <h4 className="subtitle-shops">EL CALAFATE</h4>
               </div>
               <div className="maps">
-                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12577.278143685986!2d-57.58725335176667!3d-37.99300665104608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584d92f185d0925%3A0x5020da28f3ebef8e!2sAutopartes%20Raul!5e0!3m2!1ses-419!2sar!4v1666150312503!5m2!1ses-419!2sar"
-                  className="maps-image" />
+                <Iframe
+                  url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1690.2069900646588!2d-72.27701776148953!3d-50.35450181011389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdbb0cfaead51d83%3A0x5ec8d9708467f35a!2sAlfredo%20B%C3%B3rquez%2C%20El%20Calafate%2C%20Santa%20Cruz!5e0!3m2!1ses-419!2sar!4v1667485919064!5m2!1ses-419!2sar"
+                  className="maps-image"
+                />
               </div>
               <div className="data-maps">
                 <div className="sub-maps">
                   <img className="location" src={ubicacion}></img>
                   <span className="text-location">
-                    Playa Grande, Mar del plata 7600. Buenos Aires, Argentina.
+                    Alfredo Borquez 1989, El Calafate 9405, Santa Cruz,
+                    Argentina.
                   </span>
                 </div>
                 <div className="sub-maps">
                   <img className="location" src={telefono}></img>
-                  <span className="text-location">+54 0223-4711716.</span>
+                  <span className="text-location">
+                    <a href="tel:+5492966767672">+54 9 2966 767672</a>
+                  </span>
                 </div>
               </div>
             </div>
             <div className="individual-shops">
               <div className="city-shops">
-                <h4 className="subtitle-shops">BARCELONA</h4>
+                <h4 className="subtitle-shops">RIO GALLEGOS</h4>
               </div>
               <div className="maps">
-                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3144.298579836346!2d-57.58071666590024!3d-37.993495532061104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584d9fde2e5ba29%3A0x65924f82b394e35b!2sIndustrias%20DVD%20GNC%20Express!5e0!3m2!1ses-419!2sar!4v1666233815964!5m2!1ses-419!2sar"
-                  className="maps-image" />
+                <Iframe
+                  url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9910.259193863543!2d-69.28782306044921!3d-51.612862299999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb6fdeec6298649%3A0x2b7b6268038fc48!2sMS%20DESARMADEROS!5e0!3m2!1ses-419!2sar!4v1667486377263!5m2!1ses-419!2sar"
+                  className="maps-image"
+                />
               </div>
               <div className="data-maps">
                 <div className="sub-maps">
                   <img className="location" src={ubicacion}></img>
                   <span className="text-location">
-                    Distrito de les Corts, Barcelona 08001. Cataluña, España.
+                    Esteban Maradona 958, Rio Gallegos 9400. Santa Cruz,
+                    Argentina.
                   </span>
                 </div>
                 <div className="sub-maps">
                   <img className="location" src={telefono}></img>
-                  <span className="text-location">+34 93 4711716.</span>
+                  <span className="text-location">
+                    <a href="tel:+5492966523983">+54 9 2966 523983</a>
+                  </span>
                 </div>
               </div>
             </div>
           </div>
           <div className="shops">
-            <div className="individual-shops">
+            <div className="individual-shops" style={{ width: "100%" }}>
               <div className="city-shops">
-                <h4 className="subtitle-shops">NAPLES</h4>
+                <h4 className="subtitle-shops">SANTA CLARA DEL MAR</h4>
               </div>
               <div className="maps">
-                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6290.866126745319!2d-57.55386740068057!3d-37.96702166826921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584dba6beb7aa81%3A0x7c405420b007c801!2sCheverry%20Constituci%C3%B3n!5e0!3m2!1ses-419!2sar!4v1666234226164!5m2!1ses-419!2sar"
-                  className="maps-image" />
+                <Iframe
+                  url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1083.7098932841575!2d-57.5267924504965!3d-37.828266747835414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584d3166a2d537d%3A0xf35b53055eb80f9d!2sSM%20Desarmaderos!5e0!3m2!1ses-419!2sar!4v1667486979890!5m2!1ses-419!2sar"
+                  className="maps-image"
+                />
               </div>
               <div className="data-maps">
                 <div className="sub-maps">
                   <img className="location" src={ubicacion}></img>
                   <span className="text-location">
-                    Naples, Florida B1602. United States.
+                    Los Alamos 183, Santa Clara Del Mar 7609, Buenos Aires,
+                    Argentina.
                   </span>
                 </div>
                 <div className="sub-maps">
                   <img className="location" src={telefono}></img>
-                  <span className="text-location">+081 4711716.</span>
-                </div>
-              </div>
-            </div>
-            <div className="individual-shops">
-              <div className="city-shops">
-                <h4 className="subtitle-shops">ROMA</h4>
-              </div>
-              <div className="maps">
-                <Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12577.813194054148!2d-57.56034100000001!3d-37.989886!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x48a182ef1630ca2c!2sUOCRA%20-%20Construir%20Salud%20-%20Cemap%20Mar%20del%20Plata!5e0!3m2!1ses-419!2sar!4v1666234165266!5m2!1ses-419!2sar"
-                  className="maps-image" />
-              </div>
-              <div className="data-maps">
-                <div className="sub-maps">
-                  <img className="location" src={ubicacion}></img>
                   <span className="text-location">
-                    Trastevere, Roma 00042. Italy.
+                    <a href="tel:+5492233068025">+54 9 223 3068025</a>
                   </span>
-                </div>
-                <div className="sub-maps">
-                  <img className="location" src={telefono}></img>
-                  <span className="text-location">+0039 4711716.</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <div className="section-container fourth-section">
-
-        <div className="structure">
-          <div className="internal-structure">
-            <img className="clean-img" src={clean}></img>
-          </div>
-          <div className="internal-structure">
-            <div className="tittle-structure">
-              <div className="content-structure">
-                <div className="distribution-structure1">
-                  <h2 className="tittle-content">CAR EXTERIOR CLEANING: BASIC DETAILING</h2>
-                </div>
-                <div className="distribution-structure2">
-                  <div className="subtittle-content">
-                    <h1 className="subtittle-text">SAME DAY SERVICE</h1>
-                  </div>
-                  <div className="subtittle-content"><h1 className="subtittle-text">ONLINE APPOINTMENT</h1></div>
-                  <div className="subtittle-content"><h1 className="subtittle-text">CONVENIENT LOCATION</h1></div>
-                  <div className="subtittle-content"><h1 className="subtittle-text">COMPLIMENTARY SHUTTLE</h1></div>
-                </div>
-              </div>
-
-            </div>
-          </div>
+      <div className="footer">
+        <div className="internal-footer">
+          <p className="text-footer">
+            {" "}
+            ©MSDesarmaderos Promotors Theme All rights reserved Copyrights 2022
+          </p>
         </div>
-        <div className="structure">
-          <div className="internal-structure">
-            <div className="tittle-structure">
-              <div className="content-structure">
-                <div className="distribution-structure1"><h2 className="tittle-content2">RUBBING, WAXING, AND POLISHING</h2></div>
-                <div className="distribution-structure3">
-                  <p className="last-description">Our technicians have undergone the most extensive and stringent car detail training program. And the only car that matters is yours because we will detail it to your complete satisfaction.</p>
-                </div>
-              </div>
-
-            </div>
-          </div>
-          <div className="internal-structure"> <img className="clean-img" src={clean2}></img></div>
-        </div>
-        <div className="footer">
-          <div className="internal-footer">
-            <p className="text-footer"> ©MSDesarmaderos Promotors Theme All rights reserved Copyrights 2022</p> 
-          </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       </div>
     </div>
-
   );
-
 }
 
 export default App;
